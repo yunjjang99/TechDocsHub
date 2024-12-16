437f09ff043cf97b5ec81e49368b4447c3a97bcb
yunsu <>
2024-12-16 23:20:10 +0900

    add : 글로벌 필터 추가

---
+++ backend/src/common/filters/global-excception.ts
@@ -0,0 +1,42 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+import { Request, Response } from "express";
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    if (response.headersSent) {
+      console.warn("Headers already sent, skipping response.");
+      return;
+    }
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.message
+        : "Internal server error";
+
+    // 통일된 에러 응답 형식
+    const errorResponse = {
+      isSuccess: false,
+      statusCode: status,
+      message: message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+    };
+
+    response.status(status).json(errorResponse);
+  }
+}
